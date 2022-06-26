1e6429771307213ac1c3064f43e3a5000a520310
Alexey Tarakanov <alekseytarakanov@yandex.ru>
2022-06-26 14:43:10 +0300

    Initial commit

---
+++ src/main/java/com/petproject/testspring3/Testspring3Application.java
@@ -0,0 +1,13 @@
+package com.petproject.testspring3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testspring3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testspring3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/petproject/testspring3/Testspring3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petproject.testspring3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Testspring3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
